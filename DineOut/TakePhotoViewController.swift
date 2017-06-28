3c289436e807816acfb04f4b7f88f3617cd730bf
Zhang, Xiaolong <xiaolozhang@paypal.com>
2017-06-27 18:14:51 -0700

    setup session

--- DineOut/TakePhotoViewController.swift
+++ DineOut/TakePhotoViewController.swift
@@ -10,29 +10,54 @@ import UIKit
 import AVFoundation
 
 class TakePhotoViewController: UIViewController {
-
-    let captureSession = AVCaptureSession()
+  
+  @IBOutlet weak var camPreview: UIView!
+  
+  let captureSession = AVCaptureSession()
+  var previewLayer: AVCaptureVideoPreviewLayer!
+  var activeInput: AVCaptureDeviceInput!
+  let imageOutput = AVCaptureStillImageOutput()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // Do any additional setup after loading the view.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  func setUpSession() {
+    captureSession.sessionPreset = AVCaptureSessionPresetHigh
+    let camera = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+    
+    do {
+      let input = try AVCaptureDeviceInput(device: camera)
+      if captureSession.canAddInput(input) {
+        captureSession.addInput(input)
+        activeInput = input
+      }
+    } catch {
+      print("Error setup device input: \(error)")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    imageOutput.outputSettings = [AVVideoCodecKey: AVVideoCodecJPEG]
+    
+    if captureSession.canAddOutput(imageOutput) {
+      captureSession.addOutput(imageOutput)
     }
-    */
-
+  }
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destinationViewController.
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
 }
