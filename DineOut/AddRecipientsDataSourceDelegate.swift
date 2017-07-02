47b2d546ae5f4b62de0060d698b9a793bf249a0f
Xiaolong Zhang <xlzhsteven@gmail.com>
2017-07-01 22:06:24 -0700

    Each letter change in the search bar changes the item in the table view

--- DineOut/AddRecipientsDataSourceDelegate.swift
+++ DineOut/AddRecipientsDataSourceDelegate.swift
@@ -10,20 +10,40 @@ import UIKit
 
 class AddRecipientsDataSourceDelegate: NSObject {
     
-    private weak var addRecipentsViewController: AddRecipientsViewController?
+    enum SearchState {
+        case searchActivated
+        case searchDismissed
+    }
+    
+    fileprivate weak var addRecipentsViewController: AddRecipientsViewController?
     fileprivate var addRecipentsViewModel: AddRecipientsViewModel
+    fileprivate var filteredResults: [Person] = []
+    fileprivate var searchState: SearchState = .searchDismissed
     
     init(addRecipentsVC: AddRecipientsViewController) {
         self.addRecipentsViewController = addRecipentsVC
         self.addRecipentsViewModel = AddRecipientsViewModel()
     }
+    
+    // MARK: helper method
+    func filter(from friends: [Person], with searchString: String) {
+        let filteredByNameArray = friends.filter {
+            $0.firstName.localizedCaseInsensitiveContains(searchString) || $0.lastName.localizedCaseInsensitiveContains(searchString)
+        }
+        filteredResults = filteredByNameArray
+    }
 }
 
 extension AddRecipientsDataSourceDelegate: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let friend: Person
+        if searchState == .searchActivated {
+            friend = filteredResults[indexPath.row]
+        } else {
+            friend = addRecipentsViewModel.person(withIndexPath: indexPath)
+        }
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "friendCell", for: indexPath) as! FriendTableCell
-        let friend = addRecipentsViewModel.person(withIndexPath: indexPath)
         cell.profileImageView.image = UIImage(named: friend.profileImageName!)
         cell.userNameLabel.text = "@\(friend.userName)"
         cell.nameLabel.text = "\(friend.firstName) \(friend.lastName)"
@@ -33,14 +53,23 @@ extension AddRecipientsDataSourceDelegate: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if searchState == .searchActivated {
+            return filteredResults.count
+        }
         return addRecipentsViewModel.numberOfRowInSection(section)
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
+        if searchState == .searchActivated {
+            return 1
+        }
         return addRecipentsViewModel.numberOfSection()
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if searchState == .searchActivated {
+            return "Search Results"
+        }
         return addRecipentsViewModel.titleForSection(section)
     }
 }
@@ -52,7 +81,20 @@ extension AddRecipientsDataSourceDelegate: UITableViewDelegate {
 }
 
 extension AddRecipientsDataSourceDelegate: UISearchBarDelegate {
-    override func didChangeValue(forKey key: String) {
-        print("search bar has changed value")
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchState = .searchActivated
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print("text did change to \(searchText)")
+        filter(from: addRecipentsViewModel.friends, with: searchText)
+        addRecipentsViewController?.friendsListTableView.reloadData()
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchState = .searchDismissed
+        searchBar.resignFirstResponder()
+        searchBar.text = ""
+        addRecipentsViewController?.friendsListTableView.reloadData()
     }
 }
