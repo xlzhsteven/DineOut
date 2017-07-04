0ae5183bf029ce7916828c4b72557c9fa0a5fc22
Xiaolong Zhang <xlzhsteven@gmail.com>
2017-07-04 15:23:56 -0700

    Add blur effect to the profile image

--- DineOut/FriendCollectionViewCell.swift
+++ DineOut/FriendCollectionViewCell.swift
@@ -7,10 +7,19 @@
 //
 
 import UIKit
+import QuartzCore
 
 class FriendCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var firstLastNameLabel: UILabel!
     @IBOutlet weak var userNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        profileImageView.layer.shadowColor = UIColor.darkGray.cgColor
+        profileImageView.layer.shadowOffset = CGSize(width: 0, height: 2)
+        profileImageView.layer.shadowOpacity = 1
+        profileImageView.layer.shadowRadius = 2
+        profileImageView.layer.masksToBounds = false
+    }
 }
